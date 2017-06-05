4ebf0ef05cbdd526b809bf75f493b4f193abf07d
Perry Anderson <perry.anderson@gmail.com>
2017-06-05 20:32:03 -0300

    How to get data on a Http request.

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -4,15 +4,18 @@ import { Headers, Http } from '@angular/http';
 @Injectable()
 export class ServerService {
 
+  url = 'https://udemy-ng-http-26bdc.firebaseio.com/data.json';
+
   constructor(private http: Http) {
   }
 
   storeServers(servers: any[]) {
     const headers = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.post('https://udemy-ng-http-26bdc.firebaseio.com/data.json',
-      servers,
-      { headers: headers }
-    );
+    return this.http.post(this.url, servers, { headers: headers });
+  }
+
+  getServers() {
+    return this.http.get(this.url);
   }
 
 }
